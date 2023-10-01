3de9bf03cab0e4e89621b200a048a8670e2bef16
Armand Araujo <armandaraujo1@Armands-MacBook-Air.local>
2023-09-30 19:21:55 -0700

    Corrected CSS, Reformatted Java, Quiz working

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-let questions = [
-    { question: "What comes after A ? ", answer: "B" },
-    { question: "What comes after 1 ? ", answer: "2" },
-    { question: "What comes after B ? ", answer: "C" },
-    // Add more quiz questions
-    { question: "What comes after 2 ? ", answer: "3" },
-    { question: "What comes after C ? ", answer: "D" },
 
+
+document.getElementById('startButton').addEventListener('click', startQuiz);
+
+function startQuiz() {
+    document.getElementById('startButton').style.display = 'none';
+    document.getElementById('questionContainer').style.display = 'block';
+    showQuestion();
+    timer = setInterval(updateTimer, 1000);
+}
+
+
+
+
+
+
+let questions = [
+    {question: "What comes after A ? ", answer: "B"},
+    {question: "What comes after 1 ? ", answer: "2"},
+    {question: "What comes after B ? ", answer: "C"},
+    // Add more quiz questions
+    {question: "What comes after 2 ? ", answer: "3"},
+    {question: "What comes after C ? ", answer: "D"},
+    
 ];
 
 let currentQuestion = 0;
@@ -39,10 +55,10 @@ function checkAnswer() {
 
     if (userAnswer === correctAnswer) {
         score++;
-        timeLeft += 10;
+        timeLeft += 10; 
         alert('CORRECT answer');
     } else {
-        timeLeft -= 10;
+        timeLeft -= 10; 
         alert('WRONG answer');
     }
 
@@ -60,21 +76,21 @@ function updateTimer() {
     }
 }
 
-function endGame() {
+    function endGame() {
     clearInterval(timer);
     document.getElementById('questionContainer').style.display = 'none';
-    document.getElementById('scoreContainer').style.display = 'block';
-    document.getElementById('score').textContent = score;
-    document.getElementById('initials').focus();
+        document.getElementById('scoreContainer').style.display = 'block';
+         document.getElementById('score').textContent = score;
+          document.getElementById('initials').focus();
 }
 
-document.getElementById('submitScore').addEventListener('click', submitScore);
+   document.getElementById('submitScore').addEventListener('click',     submitScore);
 
-function submitScore() {
-    let initials = document.getElementById('initials').value;
-    if (initials) {
-        let highScores = JSON.parse(localStorage.getItem('highScores')) || [];
-        highScores.push({ initials, score });
+   function submitScore() {
+      let initials = document.getElementById('initials').value;
+         if (initials) {
+           let highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+           highScores.push({ initials, score });
         highScores.sort((a, b) => b.score - a.score);
         highScores = highScores.slice(0, 5); // Keep top 5 scores
         localStorage.setItem('highScores', JSON.stringify(highScores));
@@ -92,15 +108,6 @@ function submitScore() {
 
 
 
-
-document.getElementById('startButton').addEventListener('click', startQuiz);
-
-function startQuiz() {
-    document.getElementById('startButton').style.display = 'none';
-    document.getElementById('questionContainer').style.display = 'block';
-    showQuestion();
-    timer = setInterval(updateTimer, 1000);
-}
 
 
 
