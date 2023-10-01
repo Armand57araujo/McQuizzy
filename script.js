e3190d7a511c204aa89cf6ef302d252834577ed1
Armand Araujo <armandaraujo1@Armands-MacBook-Air.local>
2023-09-30 18:10:53 -0700

    added let for questions

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ function startQuiz() {
     showQuestion();
     timer = setInterval(updateTimer, 1000);
 }
+
+
+
+let questions = [
+    {question: "What comes after A ? ", answer: "B"},
+    {question: "What comes after 1 ? ", answer: "2"},
+    {question: "What comes after B ? ", answer: "C"},
+    // Add more quiz questions
+    {question: "What comes after 2 ? ", answer: "3"},
+    {question: "What comes after C ? ", answer: "D"},
+    
+];
+
+
