a165da6a176302a031e15c774f299d11131678cf
Armand Araujo <armandaraujo1@Armands-MacBook-Air.local>
2023-09-30 18:04:57 -0700

    created and defined start button / start quiz function with timer

--- script.js
+++ script.js
@@ -2,3 +2,10 @@
 
 
 document.getElementById('startButton').addEventListener('click', startQuiz);
+
+function startQuiz() {
+    document.getElementById('startButton').style.display = 'none';
+    document.getElementById('questionContainer').style.display = 'block';
+    showQuestion();
+    timer = setInterval(updateTimer, 1000);
+}
